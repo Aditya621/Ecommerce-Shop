e909db82d3b3ed7956fa39815d72d9ed746a5a68
Shop With Aditya <thakuraditya621@gmail.com>
2021-07-14 18:03:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var menuitems = document.getElementById("menuitems");
+// menuitems.style.maxHeight = "0px";
+// //
+// functions;
+// menutoggle();
+// {
+//   if (menuitems.style.maxHeight == "0px") {
+//     menuitems.style.maxHeight = "200px";
+//   } else {
+//     menuitems.style.maxHeight = "0px";
+//   }
+// }
